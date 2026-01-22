2b38754d309df2175ae2c5e7e9b6fcd4d0bf2f5a
Pranathirevuri <pranathirevuri@gmail.com>
2026-01-22 12:42:06 +0530

    commit 8 Here i am increase gap b/w stars

--- my-tech/src/components/RatingStars.jsx
+++ my-tech/src/components/RatingStars.jsx
@@ -2,7 +2,7 @@ import { FaStar } from "react-icons/fa";
 
 export default function RatingStars({ count }) {
   return (
-    <div className="flex gap-2 text-red-500">
+    <div className="flex gap-3 text-red-500">
       {[1,2,3,4,5].map(num => (
         <FaStar key={num} className={num <= count ? "" : "opacity-30"} />
       ))}
